c99e48d0af21db384899da3d7a008b7cfac76637
Alexej Kubarev <alexej.kubarev@accedo.tv>
2020-01-02 22:19:20 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+// appVersion denotes version number of this application
+var appVersion = "x.y.z"
+
+// AppName denotes name of this application
+var appName = "~unknown~"
+
+// AppCommit denotes git commit that this binary was built from
+var appCommit = "~unknown~"
+
+// AppCommitDate denotes the date of the commit
+var appCommitDate = "~unknown~"
+
+// AppCommitTime denotes the time of the commit
+var appCommitTime = "~unknown~"
+
+func main() {
+
+}
