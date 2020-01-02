91d5ec91d8cc9f0d7e8f563d56ca506fd9d4f6aa
Alexej Kubarev <alexej.kubarev@accedo.tv>
2020-01-02 22:28:28 +0100

    Adding release workflow

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
-// appVersion denotes version number of this application
-var appVersion = "x.y.z"
-
-// AppName denotes name of this application
-var appName = "~unknown~"
-
-// AppCommit denotes git commit that this binary was built from
-var appCommit = "~unknown~"
-
-// AppCommitDate denotes the date of the commit
-var appCommitDate = "~unknown~"
-
-// AppCommitTime denotes the time of the commit
-var appCommitTime = "~unknown~"
+var (
+	appVersion    = "x.y.z"
+	appName       = "~unknown~"
+	appCommit     = "~unknown~"
+	appCommitDate = "~unknown~"
+	appCommitTime = "~unknown~"
+)
 
 func main() {
 
